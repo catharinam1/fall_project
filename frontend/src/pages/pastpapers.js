14d4c7a026981f74d390303b854dadb507621428
catharinam1 <catharinamon@gmail.com>
2023-10-24 11:46:22 -0400

    Updated FrontEnd

--- frontend/src/pages/pastpapers.js
+++ frontend/src/pages/pastpapers.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Navbar, CarouselDefault } from '../components';
-
-const PastPapers = () => {
-  return (
-    <div>
-       
-        <Navbar />
-        
-        <div className="text-white h-screen flex flex-col items-center justify-center">
-            <p className="text-4xl">Past Papers</p>
-            <p className="text-6xl">Coming Soon</p>
-            <img src="https://cdn-icons-png.flaticon.com/512/3866/3866319.png"/>
-            
-        </div>
-        
-         
-        
-    </div>
-  )
-}
-
-export default PastPapers
-=======
 import React from 'react';
 import { Navbar, Pastpaperbox } from '../components';
 import { Button, Dropdown } from 'flowbite-react';
@@ -100,4 +75,3 @@ const PastPapers = () => {
 };
 
 export default PastPapers;
->>>>>>> 2082d47 (Updated FrontEnd)
