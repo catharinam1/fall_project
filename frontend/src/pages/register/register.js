5cb16115960879a6579e28c52a98aa26258475d6
catharinam1 <catharinamon@gmail.com>
2023-10-24 13:33:13 -0400

    API for Register Maybe

--- frontend/src/pages/register/register.js
+++ frontend/src/pages/register/register.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
 import register from '../../assets/register.svg'
-
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 import { Link as Linker } from 'react-router-dom';
+import axios from "axios";
+
 
 const Register = () => {
 
@@ -16,7 +18,28 @@ const Register = () => {
     const handleOptionChange2 = (e) => {
         setSelectedOption2(e.target.value);
     };
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    })
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
+    const handleRegistration = () => {
+        axios.post("/api/register", formData) // Replace with your backend endpoint URL
+          .then((response) => {
+            // Handle the successful response (e.g., redirect to a login page)
+            console.log("Registration successful", response.data);
+          })
+          .catch((error) => {
+            // Handle registration failure (e.g., display an error message)
+            console.error("Registration failed", error);
+          });
+    };
 
     return (
         <div className="struct">
@@ -44,11 +67,12 @@ const Register = () => {
 
                             <div className="my-5">
                             <input
-    className="border border-gray-200 shadow-md py-5 px-3 my-0 rounded-md"
-    type="text"
-    placeholder="Email"
-    style={{ outline: "none" }}
-  />                            </div>
+                                className="border border-gray-200 shadow-md py-5 px-3 my-0 rounded-md"
+                                type="text"
+                                placeholder="Email"
+                                style={{ outline: "none" }}
+                            />   
+                            </div>
         
                               <div className="my-5">
                                   <input className="border border-gray-200 outline-none focus:shadow-[gray] shadow-md  py-5 px-3 my-0 rounded-md " type="password" placeholder="Password" />
@@ -58,9 +82,17 @@ const Register = () => {
                                   <input className="border border-gray-200 outline-none focus:shadow-[gray] shadow-md py-5 my-0 rounded-md" type="password" placeholder="Re-type Password" />
                               </div>
         
+                              {/* <div className="my-5 flex w-[50%]"> */}
+                                  {/* <FloatingLabel lablel ="Label" variant ="filled"/> */}
+                                  {/* <input className=" border border-gray-200 outline-none focus:shadow-[gray] shadow-md py-5 my-0 rounded-md" type="password" placeholder="Re-type Password" />
+                                  <input className=" border border-gray-200 outline-none focus:shadow-[gray] shadow-md py-5 my-0 rounded-md" type="password" placeholder="Re-type Password" />
+
+                              </div> */}
+
+
                               <div className="flex justify-center items-center grid grid-cols-2 mt-[-15px] w-full">
                                   <div className="ml-[40%] my-5 w-[55%]">
-                                      <select className="text-gray-400 border border-gray-200 border-r-0 shadow-md py-2 rounded-md flex justify-content w-full px-2" id="country" value={selectedOption} onChange={handleOptionChange}>
+                                      <select className="text-gray-400 border border-gray-200 border-r-0 shadow-md py-2 rounded-md flex justify-content w-full px-2" id="school" value={selectedOption} onChange={handleOptionChange}>
                                           <option value="">Select a school</option>
                                           <option value="i">Individual</option>
                                           <option value="qc">Queen's College</option>
@@ -76,7 +108,7 @@ const Register = () => {
                                       <select className="text-gray-400 border border-gray-200 py-2 shadow-md rounded-md flex justify-content w-full px-2" id="country" value={selectedOption2} onChange={handleOptionChange2}>
                                           <option value="">Student or Teacher</option>
                                           <option className="text-black-600" value="student">Student</option>
-                                          <option value="teacher">Teacher</option>
+                                          <option value="teacher">Educator</option>
                                       </select>
                                   </div>
                               </div>
@@ -85,10 +117,9 @@ const Register = () => {
                             
                               <div className="flex justify-center items-center">
                                   
-                              <button id="button-custom" className="w-[60%] bg-[#1896b9] hover:bg-[#126e87] rounded-md 2xl:mt-[70px] md:mt-[40px] text-[14px]"> Register </button>
+                              <button id="button-custom" className="w-[60%] bg-[#1896b9] hover:bg-[#126e87] rounded-md 2xl:mt-[70px] md:mt-[40px] text-[14px]" onClick={handleRegistration}> Register </button>
                     
                             </div>
-
                             <Linker className="hover:bg-transparent hover:before:w-0" to="/signIn">
                                 <p className="text-gray-600 mt-[30px] flex justify-center items-center text-[14px] hover:underline">Already have an account? &nbsp; <b>Sign In</b></p>
                             </Linker>
