7d2a9e9cc4dc6fda85c284ee7d8ecdc4f5ab6ee5
Keonte <knightingale@usf.edu>
2023-10-26 13:44:28 -0400

    working on signin

--- Backend/src/main/java/EduBahamas/service/loginService.java
+++ Backend/src/main/java/EduBahamas/service/loginService.java
@@ -7,32 +7,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import EduBahamas.model.student;
-import EduBahamas.model.requestBody.studentLogin;
-import EduBahamas.model.responseBody.userResponse;
+import EduBahamas.model.teacher;
+import EduBahamas.model.requestBody.userLogin;
+import EduBahamas.model.responseBody.loginResponse;
 import EduBahamas.repository.studentRepository;
+import EduBahamas.repository.teacherRepository;
 
 @Service
 public class loginService {
     private final studentRepository studentRepository;
+    private final teacherRepository teacherRepository;
 
     @Autowired
-    public loginService(studentRepository studentRepository){
+    public loginService(studentRepository studentRepository, teacherRepository teacherRepository){
         this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
     }
 
-    public Object validateUser(studentLogin studentLogin){
-        Optional<student> student = studentRepository.findStudentByEmail(studentLogin.getEmail());
-        String attemptedPassword = studentLogin.getPassword();
-        String password = student.get().getPassword();
-        Boolean passwordsMatch = BCrypt.checkpw(attemptedPassword, password);
-        
-        if(student.isPresent() && (passwordsMatch == true)){
-            userResponse message = new userResponse(true, null, student);
-            return message;
-        } else{
-            userResponse message = new userResponse(true, "incorrect email or password");
-            return message;
+    public Boolean validatePassword(String attemptedPassword, String password){
+        return BCrypt.checkpw(password, attemptedPassword);
+    }
+
+    public Object validateUser(userLogin userLogin){        
+        Optional<student> student = studentRepository.findStudentByEmail(userLogin.getEmail());
+        if(student.isPresent() == true && validatePassword(student.get().getPassword(), userLogin.getPassword()) == true){
+            return new loginResponse(student.get().getId(), "Student");
         }
+
+        Optional<teacher> teacher = teacherRepository.findTeacherByEmail(userLogin.getEmail());     
+        if(teacher.isPresent() == true && validatePassword(teacher.get().getPassword(), userLogin.getPassword()) == true){
+            return new loginResponse(teacher.get().getId(), "Teacher");
+        }
+
+        return new loginResponse(null, null);
     }
 
     public java.util.List<student> testLogin(){
