1fea53cec59d25f33ac064c87ad8f33d8a7adc5b
Keonte <knightingale@usf.edu>
2023-10-26 12:23:42 -0400

    idk frontend stuff

--- Backend/src/main/java/EduBahamas/CorsConfig.java
+++ Backend/src/main/java/EduBahamas/CorsConfig.java
@@ -1,3 +1,5 @@
+package EduBahamas;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
